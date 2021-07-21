0c4f4e6daadf9dd7234e1664c3dde5b57fc03073
Aniruddha Atale <Aniruddha.Atale@oasisworks.com>
2021-07-21 07:54:12 -0400

    Minor update -- handling Cntrl-C signal for go-lox shell

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -5,20 +5,55 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/asatale/go-lox/interpreter"
+	"io"
 	"os"
 )
 
 func Prompt() {
-	for {
-		fmt.Printf("Glox Shell>>> ")
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
 
-		source := bytes.NewBufferString(text)
-		err := interpreter.Run(source)
-		if err != nil && err != interpreter.EOFError {
-			fmt.Println("Error in interpreter: ", err)
-			return
+	sigs := make(chan os.Signal, 1)
+	data := make(chan string, 1)
+	control := make(chan struct{}, 1)
+	control <- struct{}{}
+
+	go func() {
+	Loop1:
+		for {
+			select {
+			case _, ok := <-control:
+				if !ok {
+					break Loop1
+				}
+				fmt.Printf("Glox Shell>>> ")
+				reader := bufio.NewReader(os.Stdin)
+				text, err := reader.ReadString('\n')
+				if err == io.EOF {
+					close(data)
+					break Loop1
+				}
+				data <- text
+			}
+		}
+	}()
+
+Loop2:
+	for {
+		select {
+		case <-sigs:
+			fmt.Println("Got shutdown, exiting")
+			break Loop2
+		case s, ok := <-data:
+			if !ok {
+				break Loop2
+			}
+			source := bytes.NewBufferString(s)
+			err := interpreter.Run(source)
+			if err != nil && err != interpreter.EOFError {
+				fmt.Println("Error in interpreter: ", err)
+				break Loop2
+			}
+			control <- struct{}{}
 		}
 	}
+	close(control)
 }
